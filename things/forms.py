6487669b448227c738ec96fc986dab38e1f37d7a
Vyom Fadia <vyom@tryterra.co>
2023-12-04 15:09:44 +0000

    fix: actually add form to home

--- things/forms.py
+++ things/forms.py
@@ -6,3 +6,4 @@ from django import forms
 class ThingForm(forms.Form):
     name = forms.CharField(max_length=35)
     description = forms.CharField(max_length=120, widget=forms.Textarea)
+    quantity = forms.IntegerField(min_value=0, max_value=50, widget=forms.NumberInput)
